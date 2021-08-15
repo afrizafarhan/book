07b821f25a3d35155f49c63b712524db899535bd
Han <farhanafriza24@gmail.com>
2021-08-15 22:17:40 +0700

    add author controller, author model and author routes

---
+++ models/Author.js
@@ -0,0 +1,26 @@
+const {Con} = require('../core/connection')
+const tableName = 'author'
+
+const getListAuthors = () => {
+    return Con.query(`SELECT * FROM ${tableName}`).then(res => res.rows).catch(e => e.stack)
+}
+
+const getDetailAuthor = (data) => {
+    const {id} = data.body
+    return Con.query(`SELECT * FROM ${tableName} WHERE id = $1`,[id]).then(res => res.rows).catch(e => e.stack)
+}
+
+const addAuthor = (data) => {
+    const {name, email} = data.body
+    return Con.query(`INSERT INTO ${tableName}(name, email) VALUES ($1, $2)`, [name, email])
+    .then(res => res.rowCount > 0 ? {status: 200} :{status: 404})
+}
+
+const updateAuthor = (data) = {
+
+}
+
+const deleteAuthor = (data) => {
+    const {id} = data.body
+    return Con.query(`DELETE FROM ${tableName} WHERE id = $1`,[id]).then(res => res.rowCount > 0 ? {status: 200}: {status : 404})
+}
