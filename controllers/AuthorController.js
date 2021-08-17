73bde23eca436bc1a44799869e42ebe7b298d9b8
Han <farhanafriza24@gmail.com>
2021-08-17 15:24:17 +0700

    add all function crud to master data author

--- controllers/AuthorController.js
+++ controllers/AuthorController.js
@@ -1,11 +1,35 @@
 const {json} = require('./../core/module')
 const Author = require('./../models/Author')
 
-const authorsList = async (req, res) => {
+const authorsList = async (_, res) => {
     const data = await Author.getListAuthors()
     res.status(200).json(data)
 }
 
+const detailAuthor = async(req, res) => {
+    const data = await Author.getDetailAuthor(req.body)
+    res.status(200).json(data)
+}
+
+const addAuthor = async(req, res) => {
+    const data = await Author.addAuthor(req.body)
+    res.send(data)
+}
+
+const updateAuthor = async(req, res) => {
+    const data = await Author.updateAuthor(req.body)
+    res.send(data)
+}
+
+const updateStatusAuthor = async(req, res) => {
+    const data = await Author.nonActiveAuthorData(req.body)
+    res.send(data)
+}
+
 module.exports = {
-    authorsList
+    authorsList,
+    addAuthor,
+    updateAuthor,
+    detailAuthor,
+    updateStatusAuthor
 }
