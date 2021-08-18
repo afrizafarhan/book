42519377158c9c1a6dac72b3aba51f1de11bea4b
Han <farhanafriza24@gmail.com>
2021-08-18 19:13:47 +0700

    delete cause will use form-data

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const { app, express } = require('./core/module')
-
+const { app, express} = require('./core/module')
 //routes
 const catalogRouter = require('./routes/catalog')
 
-app.use(express.urlencoded({extended:true}))
 app.use('/', catalogRouter)
 
 app.listen(process.env.APP_PORT, () => console.log(`SERVER RUNNING ON http://localhost:${process.env.APP_PORT}`))
