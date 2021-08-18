65f60a74b2f4bdca4944ec469ad9e8e07744aa3a
Han <farhanafriza24@gmail.com>
2021-08-18 19:14:45 +0700

    call some new package

--- core/module.js
+++ core/module.js
@@ -1,15 +1,22 @@
+const fs = require('fs')
 const express = require('express')
+const json = express.json
+const multer = require('multer')
 const app = express()
 const router = express.Router()
 const dotenv = require('dotenv')
 const env = dotenv.config()
-const json = express.json
-
+const upload = multer()
+const path = require('path')
 
 module.exports = {
-    express,
     app,
-    router,
     env,
-    json
+    express,
+    fs,
+    json,
+    multer,
+    path,
+    router,
+    upload
 }
