a3a9894e779b755713dc714e8777b43b62fe5ac6
Han <farhanafriza24@gmail.com>
2021-09-01 22:11:09 +0700

    change code to get response data from promise and if error change status to 500

--- src/controllers/LanguageController.js
+++ src/controllers/LanguageController.js
@@ -2,34 +2,39 @@ const Languages = require('../models/Language')
 const { json } = require('../core/module')
 
 
-const languagesList = async (_, res) => {
-    const response = await Languages.getListLanguages()
-    if(response instanceof Error) res.status(500).send(response.message)
-    else res.send(response)
+const languagesList = (_, res) => {
+    Languages.getListLanguages().then(response => {
+        if(response instanceof Error) res.status(500).send(response.message)
+        else res.send(response)
+    })
 }
 
-const addLanguage = async (req, res) => {
-    const response = await Languages.addLanguage(req.body)
-    if(response instanceof Error) res.status(500).send(response.message)
-    else res.send(response)
+const addLanguage = (req, res) => {
+    Languages.addLanguage(req.body).then(response => {
+        if(response instanceof Error) res.status(500).send(response.message)
+        else res.send(response)
+    })
 }
 
-const detailLanguage = async (req, res) => {
-    const response = await Languages.getDetailLanguage(req.body)
-    if(response instanceof Error) res.status(500).send(response.message)
-    else res.send(response)
+const detailLanguage = (req, res) => {
+    Languages.getDetailLanguage(req.body).then(response => {
+        if(response instanceof Error) res.status(500).send(response.message)
+        else res.send(response)
+    })
 }
 
-const updateLanguage = async (req, res) => {
-    const response = await Languages.updateLanguage(req.body)
-    if(response instanceof Error) res.status(500).send(response.message)
-    else res.send(response)
+const updateLanguage = (req, res) => {
+    Languages.updateLanguage(req.body).then(response => {
+        if(response instanceof Error) res.status(500).send(response.message)
+        else res.send(response)
+    })
 }
 
-const deleteLanguage = async (req, res) => {
-    const response = await Languages.deleteLanguage(req.body)
-    if(response instanceof Error) res.status(500).send(response.message)
-    else res.send(response)
+const deleteLanguage = (req, res) => {
+    Languages.deleteLanguage(req.body).then(response => {
+        if(response instanceof Error) res.status(500).send(response.message)
+        else res.send(response)
+    })
 }
 
 module.exports = {
